ab7ec3c61c018526699f25f1e7e349e6177097ee
Daniel <danielcgilibert@gmail.com>
2021-09-07 20:30:27 +0200

    Creado mutation para el User

--- backend/gql/resolver.js
+++ backend/gql/resolver.js
@@ -6,6 +6,14 @@ const resolvers = {
       return null;
     },
   },
+  Mutation: {
+    // User
+    register: (_, { input }) => {
+      console.log("Registrando Usuarios");
+      console.log(input);
+      return null;
+    },
+  },
 };
 
 module.exports = resolvers;
--- backend/gql/schema.js
+++ backend/gql/schema.js
@@ -13,9 +13,21 @@ const typeDefs = gql`
     createAt: String
   }
 
+  input UserInput {
+    name: String!
+    username: String!
+    email: String!
+    password: String!
+  }
+
   type Query {
     # User
     getUser: User
+  }
+
+  type Mutation {
+    # User
+    register(input: UserInput): User
   }
 `;
 
