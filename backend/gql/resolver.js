a34d09063138f82de83d2f8aa29b2214b6b5316d
Daniel <danielcgilibert@gmail.com>
2021-09-07 20:49:30 +0200

    Creado mutation User y comprobaciones de usuario y email

--- backend/gql/resolver.js
+++ backend/gql/resolver.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 const resolvers = {
   Query: {
     // User
@@ -8,9 +10,33 @@ const resolvers = {
   },
   Mutation: {
     // User
-    register: (_, { input }) => {
-      console.log("Registrando Usuarios");
+    register: async (_, { input }) => {
       console.log(input);
+      const newUser = input;
+      newUser.email = newUser.email.toLowerCase();
+      newUser.username = newUser.username.toLowerCase();
+
+      const { email, username, password } = newUser;
+
+      // Revisar si el email esta en uso
+      const foundEmail = await User.findOne({ email });
+      if (foundEmail) throw new Error("El email ya esta en uso");
+
+      // Revisar si el usuario esta en uso
+      const foundUsername = await User.findOne({ username });
+      if (foundUsername) throw new Error("El usuario ya esta en uso");
+
+      //Encriptar
+      //.....
+
+      try {
+        const user = new User(newUser);
+        user.save();
+        return user;
+      } catch (error) {
+        console.log(error);
+      }
+
       return null;
     },
   },
