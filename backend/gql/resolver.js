2766c798079a3233a515fa7f940151a4530f8414
Daniel <danielcgilibert@gmail.com>
2021-09-11 17:47:00 +0200

    Refactorizando código

--- backend/gql/resolver.js
+++ backend/gql/resolver.js
@@ -1,5 +1,4 @@
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const userController = require("../controller/user");
 
 const resolvers = {
   Query: {
@@ -11,36 +10,7 @@ const resolvers = {
   },
   Mutation: {
     // User
-    register: async (_, { input }) => {
-      console.log(input);
-      const newUser = input;
-      newUser.email = newUser.email.toLowerCase();
-      newUser.username = newUser.username.toLowerCase();
-
-      const { email, username, password } = newUser;
-
-      // Revisar si el email esta en uso
-      const foundEmail = await User.findOne({ email });
-      if (foundEmail) throw new Error("El email ya esta en uso");
-
-      // Revisar si el usuario esta en uso
-      const foundUsername = await User.findOne({ username });
-      if (foundUsername) throw new Error("El usuario ya esta en uso");
-
-      //Encriptar
-      const salt = await bcrypt.genSaltSync(10);
-      newUser.password = await bcrypt.hash(password, salt);
-
-      try {
-        const user = new User(newUser);
-        user.save();
-        return user;
-      } catch (error) {
-        console.log(error);
-      }
-
-      return null;
-    },
+    register: async (_, { input }) => userController.register(input),
   },
 };
 
