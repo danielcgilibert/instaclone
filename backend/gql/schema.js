2ff3dff46fc6412a0b05da484f26e820ee77a400
Daniel <danielcgilibert@gmail.com>
2021-09-13 19:42:33 +0200

    Instalado JWT

--- backend/gql/schema.js
+++ backend/gql/schema.js
@@ -13,9 +13,18 @@ const typeDefs = gql`
     createAt: String
   }
 
+  type Token {
+    token: String
+  }
+
   input UserInput {
     name: String!
     username: String!
+    email: String!
+    password: String!
+  }
+
+  input LoginInput {
     email: String!
     password: String!
   }
@@ -28,6 +37,7 @@ const typeDefs = gql`
   type Mutation {
     # User
     register(input: UserInput): User
+    login(input: LoginInput): Token
   }
 `;
 
