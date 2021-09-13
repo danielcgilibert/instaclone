a8e8ed83ff6e44a320752e4aea361c04b381b2d8
Daniel <danielcgilibert@gmail.com>
2021-09-13 19:57:49 +0200

    Creado cliente con react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "semantic-ui-react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>APP</h1>
+      <Button primary>Click Here</Button>
+    </div>
+  );
+}
+
+export default App;
