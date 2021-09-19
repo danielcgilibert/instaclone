5dbaaa81432120f2908b4c67782eda4579161c75
Daniel <danielcgilibert@gmail.com>
2021-09-19 13:17:13 +0200

    Agregado lógica de validación

--- client/src/components/Auth/RegisterForm/RegisterForm.js
+++ client/src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,13 +1,31 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
-import "./RegisterForm.scss";
 import { initialValues } from "./initialValues";
 import { useFormik } from "formik";
+import * as Yup from "yup";
+import "./RegisterForm.scss";
 
 export const RegisterForm = ({ setShowLogin }) => {
   const formik = useFormik({
     initialValues: initialValues(),
-    validationSchema: null,
+    validationSchema: Yup.object({
+      name: Yup.string().required("El nombre es obligatorio"),
+      username: Yup.string()
+        .matches(
+          /^[a-zA-Z0-9-]*$/,
+          "El nombre del usuario no puede tener espacio"
+        )
+        .required("El nombre de usuario es obligatorio"),
+      email: Yup.string()
+        .email("El email no es valido")
+        .required("El email es obligatorio"),
+      password: Yup.string()
+        .required("La contraseña es obligatoria")
+        .oneOf([Yup.ref("password2")], "Las contraseña no son iguales"),
+      password2: Yup.string()
+        .required("La contraseña es obligatoria")
+        .oneOf([Yup.ref("password")], "Las contraseña no son iguales"),
+    }),
     onSubmit: (formValue) => {
       console.log(formValue);
     },
@@ -24,6 +42,7 @@ export const RegisterForm = ({ setShowLogin }) => {
           placeholder="Nombre y apellidos"
           name="name"
           onChange={formik.handleChange}
+          error={formik.errors.name && true}
         />
 
         <Form.Input
@@ -31,12 +50,14 @@ export const RegisterForm = ({ setShowLogin }) => {
           placeholder="Nombre de usuario"
           name="username"
           onChange={formik.handleChange}
+          error={formik.errors.username && true}
         />
         <Form.Input
           type="email"
           placeholder="Correo electronico"
           name="email"
           onChange={formik.handleChange}
+          error={formik.errors.email && true}
         />
 
         <Form.Input
@@ -44,6 +65,7 @@ export const RegisterForm = ({ setShowLogin }) => {
           placeholder="Contraseña"
           name="password"
           onChange={formik.handleChange}
+          error={formik.errors.password && true}
         />
 
         <Form.Input
@@ -51,9 +73,12 @@ export const RegisterForm = ({ setShowLogin }) => {
           placeholder="Repetir contraseña"
           name="password2"
           onChange={formik.handleChange}
+          error={formik.errors.password2 && true}
         />
 
-        <Button className="btn-submit">Registrarse</Button>
+        <Button type="submit" className="btn-submit">
+          Registrarse
+        </Button>
       </Form>
     </>
   );
