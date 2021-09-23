d9d50c6e7044602f2a01448500d28a35d904b765
Daniel <danielcgilibert@gmail.com>
2021-09-23 18:25:28 +0200

    Creado Header y añadido al Layout Basic

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Grid, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/2.1 instaclone.png";
+import "./Header.scss";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <Container>
+        <Grid>
+          <Grid.Column width={3} className="header__logo">
+            <Link to="/">
+              <Image src={Logo} alt="Instaclone" />
+            </Link>
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <p>Buscador</p>
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <p>Options</p>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
--- client/src/layouts/LayoutBasic.js
+++ client/src/layouts/LayoutBasic.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
+import { Header } from "../components/Header/Header";
 
 export default function LayoutBasic(props) {
   const { children } = props;
   return (
     <>
-      <h1>Header</h1>
+      <Header />
       <Container className="layout-basic">{children}</Container>
     </>
   );
