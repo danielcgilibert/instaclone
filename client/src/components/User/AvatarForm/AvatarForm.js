9b062e35717b2d3401bb536e294d870fda0ce53c
Daniel Carmona <danielcgilibert@gmail.com>
2021-09-25 17:08:55 +0200

    Implementando react-dropzone

--- client/src/components/User/AvatarForm/AvatarForm.js
+++ client/src/components/User/AvatarForm/AvatarForm.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Button } from "semantic-ui-react";
-
+import { useDropzone } from "react-dropzone";
 import "./AvatarForm.scss";
 export const AvatarForm = ({ setShowModal }) => {
+  const onDrop = useCallback((acceptedFile) => {
+    console.log(acceptedFile);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/jpeg, image/png",
+    noKeyboard: true,
+    multiple: false,
+    onDrop,
+  });
+
   return (
     <div className="avatar-form">
-      <Button>Cargar una foto</Button>
+      <Button {...getRootProps()}>Cargar una foto</Button>
       <Button>Eliminar foto actual</Button>
       <Button onClick={() => setShowModal(false)}>Cancelar</Button>
+      <input {...getInputProps()} />
     </div>
   );
 };
