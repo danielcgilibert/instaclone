eb735049b50bb69e63558dc848d1d45acee11f26
Daniel Carmona <danielcgilibert@gmail.com>
2021-09-26 12:09:30 +0200

    Agregando subir avatares

--- client/src/components/User/AvatarForm/AvatarForm.js
+++ client/src/components/User/AvatarForm/AvatarForm.js
@@ -1,10 +1,23 @@
 import React, { useCallback } from "react";
 import { Button } from "semantic-ui-react";
 import { useDropzone } from "react-dropzone";
+import { useMutation } from "@apollo/client";
+import { UPDATE_AVATAR } from "../../../gql/user";
 import "./AvatarForm.scss";
+
 export const AvatarForm = ({ setShowModal }) => {
-  const onDrop = useCallback((acceptedFile) => {
-    console.log(acceptedFile);
+  const [updateAvatar] = useMutation(UPDATE_AVATAR);
+
+  const onDrop = useCallback(async (acceptedFile) => {
+    const file = acceptedFile[0];
+    console.log(file);
+
+    try {
+      const result = await updateAvatar({ variables: { file } });
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 
   const { getRootProps, getInputProps } = useDropzone({
